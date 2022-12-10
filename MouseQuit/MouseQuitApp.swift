33b89994f14f37508c20d152dc299aea8aa50d42
Aditya Maramreddy <adityamaramreddy@icloud.com>
2022-12-10 13:34:57 -0800

    Initial Commit

---
+++ MouseQuit/MouseQuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseQuitApp.swift
+//  MouseQuit
+//
+//  Created by Aditya Maramreddy on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MouseQuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
